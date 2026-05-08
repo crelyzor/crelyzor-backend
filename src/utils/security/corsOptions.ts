f8320b72219fbad426ffa3785e70c37195ecce9f
hrshkshri <harshkeshari100@gmail.com>
2026-05-08 12:57:02 +0530

    fix(cors): allow admin portal origin localhost:5175

--- src/utils/security/corsOptions.ts
+++ src/utils/security/corsOptions.ts
@@ -5,6 +5,7 @@ const localhostOrigins =
     ? [
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://localhost:5175",
         "http://localhost:3000",
       ]
     : [];
